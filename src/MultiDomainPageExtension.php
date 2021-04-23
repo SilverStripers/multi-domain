285ca8e621712833f925b081e7ef833ea6dd9009
Priyashantha Hp <priyashantha@silverstripers.com>
2021-04-23 12:18:42 +0530

    update links

---
+++ src/MultiDomainPageExtension.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace SilverStripe\MultiDomain;
+
+use SilverStripe\ORM\DataExtension;
+
+class MultiDomainPageExtension extends DataExtension
+{
+    public function updateRelativeLink(&$base, &$action)
+    {
+        $domains = MultiDomain::config()->domains;
+        if (
+            isset($_SERVER['SERVER_NAME'])
+            && ($serverName = $_SERVER['SERVER_NAME'])
+            && ($primaryDomain = $domains['primary'])
+            && $primaryDomain['hostname'] != $serverName
+        ) {
+            foreach ($domains as $domainKey => $domain) {
+                $arrNameParts = explode('.', $serverName);
+                if (
+                    $serverName == $domain['hostname']
+                    && ($resolvesTo = $domain['resolves_to'])
+                ) {
+                    $base = str_replace($resolvesTo, '', $base);
+                    break;
+                }
+            }
+        }
+    }
+}
